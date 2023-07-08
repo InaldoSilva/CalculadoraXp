88f8e26b068a5c6e1a545c7fdab6133cf2041244
Inaldo Silva <104039452+InaldoSilva@users.noreply.github.com>
2023-07-07 23:12:35 -0300

    Xp calculator. Calcula o xp utilizando uma medida base quando o jogador completar até 100 missões, e com bonus de xp quando o jogador ultrapassar 100 missões, mas mantendo xp base nas primeiras 100 missoões, e aplicando xp extra às demais.

---
+++ src/CalculadoraXP.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CalculadoraXP {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o número total de missões: ");
+        int totalMissoes = scanner.nextInt();
+
+        int xpBase = 50;
+        int xpTotal = 0;
+
+        for (int missao = 1; missao <= totalMissoes; missao++) {
+            if (missao <= 100) {
+                xpTotal = missao * xpBase;
+            } else {
+                xpTotal = 100 * xpBase + (missao - 100) * (xpBase * 2);
+            }
+        }
+
+        System.out.println("Total de XP ganho: " + xpTotal);
+
+        scanner.close();
+    }
+}
+
